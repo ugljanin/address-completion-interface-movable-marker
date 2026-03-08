352c5b7916c0ec5c0360b5c790e4124dc9a350ce
Emir Ugljanin <emirugljanin@gmail.com>
2026-03-08 15:08:08 +0100

    Update metadata

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
-	id: 'directus-labs-address-completion-interface',
+	id: 'ugljanin-address-completion-interface',
 	name: 'Address completion',
 	icon: 'box',
 	description: 'Use Google Places autocomplete Data API as an Address Completion interface!',
